47bb6300650e4a2e74d4dd9334a7c530bd4ed825
Imeguras <imeguras@cock.li>
2024-08-10 18:44:51 +0100

    More stuff

--- CANOPEN_db.h
+++ CANOPEN_db.h
@@ -23,6 +23,7 @@
 #define SDO_REQUEST_ADDRESS_COBID(node_id) (node_id+0x600)
 #define SDO_RESPONSE_ADDRESS_COBID(node_id) (node_id+0x580)
 
+
 /**======================================================================**/
 
 /**================== Receive from World ==========================**/
@@ -45,12 +46,15 @@
 #define MAP_DECODE_PDO_RXFOUR_MAXON_CONTROL_WORD(x) (x[1] << 8 | x[0])
 #define MAP_DECODE_PDO_RXFOUR_MAXON_TARGET_SPEED(x) (x[5] << 24 | x[4] << 16 | x[3] << 8 | x[2])
 
-/**=================== Maxon Transmit to World ==========================**/
+/**=================== Transmit to World ==========================**/
 #define PDO_TXONE(node_id) (node_id+0x180)
 /**======================================================================**/
 #define MAP_DECODE_PDO_TXONE_MAXON_STATUSWORD(x) (x[1] << 8 | x[0])
 #define MAP_DECODE_PDO_TXONE_MAXON_MODES_OF_OPERATION(x) (x[2])
 #define MAP_DECODE_PDO_TXONE_MAXON_ERROR_CODE(x) (x[4] << 8 | x[3])
+
+#define MAP_DECODE_PDO_TXONE_RES_STATUS(x) (x[0])
+
 
 /**======================================================================**/
 #define PDO_TXTWO(node_id) (node_id+0x280)
